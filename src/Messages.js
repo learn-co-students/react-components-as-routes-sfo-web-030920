c0a5ee31f650e2d7438cd7d964ebb7ee06671316
caicindy87 <caicindy87@gmail.com>
2020-05-20 17:22:58 -0700

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Messages = () => <h2>Messages</h2>;
+
+export default Messages;
---
+++ src/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+const NavBar = () => (
+  <div>
+    {/* set exact so it knows to only set activeStyle when route is deeply equal to link  */}
+    <NavLink
+      to="/"
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */ activeStyle={{ background: "darkblue" }}
+    >
+      Home
+    </NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{ background: "darkblue" }}
+    >
+      About
+    </NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{ background: "darkblue" }}
+    >
+      Login
+    </NavLink>
+    <NavLink
+      to="/signup"
+      exact
+      style={link}
+      activeStyle={{ background: "darkblue" }}
+    >
+      Sign Up
+    </NavLink>
+    <NavLink
+      to="/messages"
+      exact
+      style={link}
+      activeStyle={{ background: "darkblue" }}
+    >
+      Messages
+    </NavLink>
+  </div>
+);
+
+export default NavBar;
